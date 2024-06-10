e12ae93cb8fc177123e387d0c971404de3e0efa0
Ivan Zyulin <solar.ivz@gmail.com>
2024-06-10 23:51:29 +1200

    add selenium

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from selenium.webdriver.common.by import By
 import time
 
 
+# Инициализация веб-драйвера Chrome
+browser = webdriver.Chrome(executable_path="C:/chromedriver/chromedriver.exe")
+
+# Переход на главную страницу Википедии
+browser.get("https://ru.wikipedia.org/wiki/Заглавная_страница")
