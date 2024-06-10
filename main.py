284af3650d080daf024489bc33cfada7d93446ab
Ivan Zyulin <solar.ivz@gmail.com>
2024-06-11 00:08:11 +1200

    add func and cycle

--- main.py
+++ main.py
@@ -1,11 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 import time
 
 
-# Инициализация веб-драйвера Chrome
-browser = webdriver.Chrome(executable_path="C:/chromedriver/chromedriver.exe")
+# Функция для ввода запроса и перехода на страницу Википедии
+def search_wikipedia(query):
+    search_box = browser.find_element(By.ID, "searchInput")
+    search_box.clear()
+    search_box.send_keys(query)
+    search_box.send_keys(Keys.RETURN)
+    time.sleep(3)
+
+
+
+# Инициализация веб-драйвера Chrome с использованием Service
+service = Service('C:/chromedriver/chromedriver.exe')
+browser = webdriver.Chrome(service=service)
 
 # Переход на главную страницу Википедии
 browser.get("https://ru.wikipedia.org/wiki/Заглавная_страница")
+
+# Основной цикл программы
+while True:
+    # Спрашиваем у пользователя запрос
+    query = input("Введите ваш запрос (или 'exit' для выхода): ")
+    if query.lower() == 'exit':
+        break
+
+    # Выполняем поиск на Википедии
+    search_wikipedia(query)
+
+    while True:
+        # Предлагаем пользователю действия
+        print("\nВыберите действие:")
+        print("1. Листать параграфы текущей статьи")
+        print("2. Перейти на одну из связанных страниц")
+        print("3. Выйти из программы")
+        action = input("Введите номер действия: ")
+
+        if action == '1':
+            print_paragraphs()
+        elif action == '2':
+            hatnotes = print_related_links()
